a38f7408fd8f8de58020e0dd4d40bd4d4ac2d7fb
tw1xxxxx <133611442+tw1xxxxx@users.noreply.github.com>
2023-09-18 01:04:27 +0300

    сношу все
    Для нормальной работы калькулятора в этом языке, как  я понял, нужно использовать строчек 50 условий. На других языках все работает вообще иначе.

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,51 +2,60 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-namespace ConsoleApp1
+class Program
 {
-    class Program
+
+    static void Main()
     {
-        static string a = " ";
-        static int b=0, c = 0;
-        int[] array1 = { };
-        int[] array2 = { };
-
-
-        static void Main()
+        List<int> arr = new List<int>();
+        List<string> err = new List<string>();
+        Console.WriteLine("Вас приветствует Практическая работа 1");
+        Console.WriteLine("Это небольшой словарь калькулятора:");
+        Console.ReadKey();
+        Console.WriteLine("(+) - Прибавить.");
+        Console.WriteLine("(-) - Отнять.");
+        Console.WriteLine("(*) - Умножить.");
+        Console.WriteLine("(/) - Разделить.");
+        Console.WriteLine("(sqrt) - Найти квадратный корень.");
+        Console.WriteLine("(**arg) - Возвести в степень.");
+        Console.WriteLine("(%%arg) - Найти процент от числа.");
+        Console.WriteLine("(!) - Факториал.");
+        Console.WriteLine("Введите удобное число-команду для вывода результата");
+        int stop = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("- Результат.");
+        int result = 0;
+        int a = 0;
+        while (a != stop)
         {
-            //for (int i =0; i > length; )
-            Console.WriteLine("Введи число ");
-            string txt = Console.ReadLine();
-            Console.WriteLine("Введи знак ");
-            a = Console.ReadLine();
-            Console.WriteLine("Введи число ");
-            while (int.TryParse(txt, out b))
-            {
-                int[] array1 = {};
-                int[] array2 = {};
-
+            Console.WriteLine("Введите число");
+            a = Convert.ToInt32(Console.ReadLine());
+            arr.Add(a);
+            Console.WriteLine("Введите знак");
+            if(a != stop) {
+                string b = Convert.ToString(Console.ReadLine());
+                err.Add(b);
             }
-            Console.ReadLine();
-                if (a == "-")
-            {
-                Console.WriteLine(b - c);
-            }
-           
-            if (a == "+")
-            {
-                Console.WriteLine(b + c);
-            }
-            
-            if (a == "*")
-            {
-                Console.WriteLine(b * c);
-            }
-            if (a == "/")
-            {
-                Console.WriteLine(b / c);
-            }
-            Console.ReadKey();
         }
+        for (var i = 0; i != arr.Count-1; i++)
+        {
+            if (err[i] == "+")
+            {
+                result += arr[i];
+            }
+            else if (err[i] == "-")
+            {
+                result -= arr[i];
+            }
+            else if (err[i] == "*")
+            {
+                result *= arr[i];
+            }
+            else if (err[i] == "/")
+            {
+                result = arr[i] / arr[i+1];
+            }
+        }    
+        Console.WriteLine(result);
+        Console.ReadKey();
     }
 }
